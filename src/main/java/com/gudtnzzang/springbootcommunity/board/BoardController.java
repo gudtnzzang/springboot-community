16610c3b333330db03f2ff99e9abd7a80b6c93ce
gudtnzzang <hs950823@gmail.com>
2021-03-08 14:33:17 +0900

    게시글 목록 조회 기능 추가

--- src/main/java/com/gudtnzzang/springbootcommunity/board/BoardController.java
+++ src/main/java/com/gudtnzzang/springbootcommunity/board/BoardController.java
@@ -3,8 +3,11 @@ package com.gudtnzzang.springbootcommunity.board;
 import com.gudtnzzang.springbootcommunity.board.dto.BoardDto;
 import com.gudtnzzang.springbootcommunity.board.service.BoardService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class BoardController {
@@ -15,7 +18,9 @@ public class BoardController {
     }
 
     @GetMapping("/")
-    public String list() {
+    public String list(Model model) {
+        List<BoardDto> boardDtoList = boardService.getBoardList();
+        model.addAttribute("postList", boardDtoList);
         return "board/list.html";
     }
 
--- src/main/java/com/gudtnzzang/springbootcommunity/board/service/BoardService.java
+++ src/main/java/com/gudtnzzang/springbootcommunity/board/service/BoardService.java
@@ -1,10 +1,13 @@
 package com.gudtnzzang.springbootcommunity.board.service;
 
+import com.gudtnzzang.springbootcommunity.board.domain.entity.Board;
 import com.gudtnzzang.springbootcommunity.board.domain.repository.BoardRepository;
 import com.gudtnzzang.springbootcommunity.board.dto.BoardDto;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class BoardService {
@@ -17,5 +20,23 @@ public class BoardService {
     @Transactional
     public Long savePost(BoardDto boardDto) {
         return boardRepository.save(boardDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public List<BoardDto> getBoardList() {
+        List<Board> boardList = boardRepository.findAll();
+        List<BoardDto> boardDtoList = new ArrayList<>();
+
+        for(Board board : boardList) {
+            BoardDto boardDto = BoardDto.builder()
+                    .id(board.getId())
+                    .author(board.getAuthor())
+                    .title(board.getTitle())
+                    .content(board.getContent())
+                    .createdDate(board.getCreatedDate())
+                    .build();
+            boardDtoList.add(boardDto);
+        }
+        return boardDtoList;
     }
 }
