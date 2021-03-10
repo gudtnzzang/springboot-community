d1108fd91cdf64fa912f8cfafe7a99d771552f8d
gudtnzzang <hs950823@gmail.com>
2021-03-10 19:29:25 +0900

    signup 페이지 수정

--- src/main/java/com/gudtnzzang/springbootcommunity/board/controller/UserController.java
+++ src/main/java/com/gudtnzzang/springbootcommunity/board/controller/UserController.java
@@ -3,7 +3,9 @@ package com.gudtnzzang.springbootcommunity.board.controller;
 import com.gudtnzzang.springbootcommunity.board.dto.UserDto;
 import com.gudtnzzang.springbootcommunity.board.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
@@ -12,10 +14,28 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
-    @PostMapping("/user")
+    @GetMapping("/signup")
+    public String displaySignup() {
+        return "board/signup.html";
+    }
+    @PostMapping("/signup")
     public String signup(UserDto userDto) {
         userService.save(userDto);
         return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String displayLogin() {
+        return "board/login.html";
+    }
+
+    @GetMapping("/user/info")
+    public String dispMyInfo() {
+        return "myinfo";
+    }
+
+    @GetMapping("/admin")
+    public String dispAdmin() {
+        return "admin";
     }
 }
