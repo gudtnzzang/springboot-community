433f3f05d988cdb9b58d963c8866606db122b3e8
gudtnzzang <hs950823@gmail.com>
2021-03-10 19:45:44 +0900

    로그아웃 기능 추가

--- src/main/java/com/gudtnzzang/springbootcommunity/board/controller/UserController.java
+++ src/main/java/com/gudtnzzang/springbootcommunity/board/controller/UserController.java
@@ -2,10 +2,15 @@ package com.gudtnzzang.springbootcommunity.board.controller;
 
 import com.gudtnzzang.springbootcommunity.board.dto.UserDto;
 import com.gudtnzzang.springbootcommunity.board.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class UserController {
@@ -37,5 +42,11 @@ public class UserController {
     @GetMapping("/admin")
     public String dispAdmin() {
         return "admin";
+    }
+
+    @GetMapping(value = "/logout")
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 }
