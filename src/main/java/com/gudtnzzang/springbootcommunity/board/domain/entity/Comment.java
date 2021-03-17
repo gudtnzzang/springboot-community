a11ec2edef83fbfba7e321a710c4844d21e85d38
gudtnzzang <hs950823@gmail.com>
2021-03-17 15:02:50 +0900

    Comment entity 추가

---
+++ src/main/java/com/gudtnzzang/springbootcommunity/board/domain/entity/Comment.java
@@ -0,0 +1,48 @@
+package com.gudtnzzang.springbootcommunity.board.domain.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Comment {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+    @Builder
+    public Comment(Long id, Post post, User user, String content) {
+        this.id = id;
+        this.post = post;
+        this.user = user;
+        this.content = content;
+    }
+}
